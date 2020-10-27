4479994f206c97643cf936d1e7ced30f35abae66
FalconRabbit233 <34422044+FalconRabbit233@users.noreply.github.com>
2020-10-27 22:35:08 +0100

    s_karte写入beschreibung字段

--- src/MiniBuffet/Controller/InitController.php
+++ src/MiniBuffet/Controller/InitController.php
@@ -12,14 +12,43 @@ class InitController extends RestController
 {
     public function run()
     {
+        $PRODUCT_TABLE_NAME = 's_karte';
+        $DESCRIPTION_COLUMN_NAME = 'beschreibung';
+
         $ORDER_TABLE_NAME = 'buffet_order';
         $ORDER_DETAIL_TABLE_NAME = 'buffet_order_detail';
         $SETTING_TABLE_NAME = 'buffet_setting';
 
+        $COLUMN_TIP_EXIST = 'column exists, abort creating';
         $TABLE_TIP_EXIST = 'table exists, abort creating';
+        $TABLE_TIP_NOT_EXIST = 'table not exists, abort creating';
         $TABLE_TIP_CREATED = 'done';
 
         $init_status = array();
+
+        if (Manager::schema()->hasTable($PRODUCT_TABLE_NAME)) {
+            $columns = Manager::connection()->select(<<<SQL
+select column_name from information_schema.columns where table_schema = ? and table_name = ?
+SQL
+                , array($this->app->env['db']['database'], $PRODUCT_TABLE_NAME));
+            $column_names = array_map(function ($item) {
+                return $item['column_name'];
+            }, $columns);
+
+            if (in_array($DESCRIPTION_COLUMN_NAME, $column_names)) {
+                $init_status["$PRODUCT_TABLE_NAME.$DESCRIPTION_COLUMN_NAME"] = $COLUMN_TIP_EXIST;
+            } else {
+                Manager::schema()->table($PRODUCT_TABLE_NAME, function ($table) use ($DESCRIPTION_COLUMN_NAME) {
+                    /** @var Blueprint $table */
+                    $table->text($DESCRIPTION_COLUMN_NAME)->default('');
+                });
+
+                $init_status["$PRODUCT_TABLE_NAME.$DESCRIPTION_COLUMN_NAME"] = $TABLE_TIP_CREATED;
+            }
+
+        } else {
+            $init_status["$PRODUCT_TABLE_NAME.$DESCRIPTION_COLUMN_NAME"] = $TABLE_TIP_NOT_EXIST;
+        }
 
         if (Manager::schema()->hasTable($ORDER_TABLE_NAME)) {
             $init_status[$ORDER_TABLE_NAME] = $TABLE_TIP_EXIST;
